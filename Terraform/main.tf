6a71dc1a6ffeaa0165fff2dd2ebb4063442bc763
Rajesh-210 <rajeshchilukuri210@gmail.com>
2026-06-02 07:56:53 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -41,8 +41,8 @@ resource "aws_security_group" "ec2_security_group" {
 }
 
 resource "aws_instance" "Monitoring_server" {
-ami = "ami-00bb6a80f01f03502"  
-instance_type = "t2.medium"
+ami = "ami-07a00cf47dbbc844c"  
+instance_type = "m7i-flex.large"
 security_groups = [aws_security_group.ec2_security_group.name]
 key_name = var.key_name
 tags = {
